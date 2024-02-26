cb5c41ce6e30b89dc22d241d76c80950e4da7688
Mario Concha <profemario.mc@gmail.com>
2024-02-26 20:47:55 -0300

    todo_list_react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState ("");
+  const [todoList, setTodoList] = useState([]);
+  console.log (inputValue);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="todolistcard">
+        <div className="todo"><h1>todos</h1></div>
+        <ul>
+          <li>
+            <input
+              type="text"
+              placeholder="What needs to be done?"
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+              onKeyDown={(e)=> e.key === 'Enter' ? setTodoList(todoList.concat(inputValue), setInputValue("")) : null}
+            ></input>
+          </li>
+          {todoList.map((todoItem, index) => (
+            <li key={index} className="li_css">
+              <div>{todoItem}</div> <div><i className="fa-solid fa-x" onClick={()=> setTodoList(todoList.filter(item => item !== todoItem))}></i></div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div id="tasks_left">{todoList.length} item left</div>
     </div>
   );
 }
