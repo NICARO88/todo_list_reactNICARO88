9b9274613823451ac213a9b5cd2fca96bc7cd328
Mario Concha <profemario.mc@gmail.com>
2024-04-10 22:16:27 -0400

    todo list corregida

--- src/App.js
+++ src/App.js
@@ -1,10 +1,100 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [inputValue, setInputValue] = useState ("");
+  const [inputValue, setInputValue] = useState("");
   const [todoList, setTodoList] = useState([]);
-  console.log (inputValue);
+
+  function obtenerTareas() {
+    fetch("https://playground.4geeks.com/todo/users/Daniel", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch todo list');
+        }
+        return response.json();
+      })
+    .then(data => setTodoList(Array.isArray(data)? data : []))
+    .catch(error => console.error(error));
+  }
+  
+
+  useEffect(() => {
+    obtenerTareas();
+  }, [])
+
+  function agregarTareas() {
+    if (inputValue.trim()!== "") {
+      fetch("https://playground.4geeks.com/todo/todos/Daniel", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ label: inputValue, is_done: false }),
+      })
+     .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to add todo');
+          }
+          return response.json();
+        })
+     .then(data => {
+          setTodoList([...todoList, data]);
+          setInputValue("");
+        })
+     .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+  
+
+  function quitarTarea(index) {
+    const todoListActualizada = todoList.filter((elemento, indice)=>indice!== index);
+    
+  
+    fetch("https://playground.4geeks.com/todo/todos/" + todoList[index].id, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(todoListActualizada.length > 0? todoListActualizada:[{label:"example task", is_done:false}]),
+    })
+   .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to modify todo');
+        }
+        return response.text(); 
+      })
+   .then(data => {
+        console.log(data); 
+        setTodoList(todoListActualizada);
+      })
+   .catch(error => {
+        console.error(error); 
+      });
+  }
+
+  function borrarTodasLasTareas() {
+    let ids = todoList.map((todoItem) => todoItem.id);
+    ids.forEach((id) => {
+      fetch("https://playground.4geeks.com/todo/todos/" + id, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to clean todos");
+          }
+          return response.text();
+        })
+        .then((data) => {
+          setTodoList([]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  }
+
   return (
     <div className="container">
       <div className="todolistcard">
@@ -16,17 +106,27 @@ function App() {
               placeholder="What needs to be done?"
               onChange={(e) => setInputValue(e.target.value)}
               value={inputValue}
-              onKeyDown={(e)=> e.key === 'Enter' ? setTodoList(todoList.concat(inputValue), setInputValue("")) : null}
+              onKeyDown={(event) => {
+                if (event.key === "Enter" && inputValue.trim()!== "") {
+                  event.preventDefault();
+                  agregarTareas(inputValue);
+                }
+              }}
+              
             ></input>
           </li>
-          {todoList.map((todoItem, index) => (
+          {Array.isArray(todoList) && todoList.map((todoItem, index) => (
             <li key={index} className="li_css">
-              <div>{todoItem}</div> <div><i className="fa-solid fa-x" onClick={()=> setTodoList(todoList.filter(item => item !== todoItem))}></i></div>
+              <div>{todoItem.label}</div>
+              <div>
+                <i className="fa-solid fa-x" onClick={() => quitarTarea(index)}></i>
+              </div>
             </li>
           ))}
         </ul>
       </div>
       <div id="tasks_left">{todoList.length} item left</div>
+      <button onClick={borrarTodasLasTareas}>Clean All</button>
     </div>
   );
 }
